4b94db39c55260ed4c6b515e5a2a4dc438f498d1
doferfan1 <dofer.wii@gmail.com>
2025-03-06 22:15:29 -0600

    hmd
    primerdeb

---
+++ ebac/Assets/scrips/holamundo.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;   
+public class holamundo: MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("hola mundo");
+        print("print");
+        Debug.LogWarning("hola mundo");
+        Debug.LogError("hola mundo");
+
+    }
+
+    void Update()
+    {
+
+    }
+}
+
