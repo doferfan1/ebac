22421d45f479e977c61fe81a51bc20f5b4383726
doferfan1 <dofer.wii@gmail.com>
2025-03-08 21:01:31 -0600

    agregar
    practica5

--- ebac/Assets/scrips/holamundo.cs
+++ ebac/Assets/scrips/holamundo.cs
@@ -29,7 +29,7 @@ public class holamundo: MonoBehaviour
 
     private void LateUpdate()
     {
-        Debug.LogWarning("hola late ");
+        Debug.LogError("hola late ");
     }
 
     private void OnEnable()
